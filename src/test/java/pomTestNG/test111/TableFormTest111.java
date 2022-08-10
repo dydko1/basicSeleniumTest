825993ff3debabd71f713622d28d2032494cbac2
dydko <miroslaw.dyduch@gmail.com>
2022-08-10 14:06:10 +0200

    testng test1

---
+++ src/test/java/pomTestNG/test111/TableFormTest111.java
@@ -0,0 +1,17 @@
+package pomTestNG.test111;
+
+import org.testng.annotations.Test;
+import pomTestNG.pages111.TablePage111;
+import pomTestNG.test111.base.BaseTest111;
+
+public class TableFormTest111 extends BaseTest111 {
+
+    @Test(priority = 0)
+    void fillForm1() throws InterruptedException {
+        TablePage111 formPage11 = new TablePage111(driver);
+
+        formPage11.fillForm11();
+        Thread.sleep(1500);
+    }
+
+}
