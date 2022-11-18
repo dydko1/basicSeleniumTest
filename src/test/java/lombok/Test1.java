d3dc32307209304a59c6e83c141ae22d35e53cce
Miroslaw Dyduch <miroslaw.dyduch@akmf.pl>
2022-11-18 14:02:04 +0100

    lombok provider

--- src/test/java/lombok/Test1.java
+++ src/test/java/lombok/Test1.java
@@ -1,12 +1,15 @@
 package lombok;
 
 public class Test1 {
-    static TestData testData; //=new TestData();
+
     public static void main(String[] args) {
 
-        testData.name="Miro";
-        testData.lastname="Dydko";
+        TestData testData = new TestData(); // jako static
+        testData.name = "Miro";
+        testData.lastname = "Dydko";
+
 
         System.out.println(testData.toString());
+        System.out.println("sss=" + testData.lists.get(1));
     }
 }
--- src/test/java/lombok/TestData.java
+++ src/test/java/lombok/TestData.java
@@ -1,7 +1,13 @@
 package lombok;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Data
 public class TestData {
     String name;
     String lastname;
+
+    List<String> lists = Arrays.asList("1111", "ddddd", "dd");
 }
