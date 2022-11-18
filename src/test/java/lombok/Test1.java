cefa7a9a951a2e107b5b2b79c5222c4d21fc1ac0
Miroslaw Dyduch <miroslaw.dyduch@akmf.pl>
2022-11-18 16:22:02 +0100

    gson format added

--- src/test/java/lombok/Test1.java
+++ src/test/java/lombok/Test1.java
@@ -1,15 +1,32 @@
 package lombok;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Test1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         TestData testData = new TestData(); // jako static
-        testData.name = "Miro";
-        testData.lastname = "Dydko";
+        //testData.name = "Miro";
+        //testData.lastname = "Dydko";
 
 
         System.out.println(testData.toString());
         System.out.println("sss=" + testData.lists.get(1));
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create(); //new Gson();
+        String jsonString = gson.toJson(testData);
+        System.out.println(jsonString);
+
+        FileWriter fileWriter = new FileWriter("plik2.json");
+        fileWriter.write(jsonString);
+        fileWriter.close();
+//
+//        User user = new User(1, "Tom Smith", "American");
+//        gson.toJson(user, new FileWriter("plik2.json"));
     }
 }
--- src/test/java/lombok/TestData.java
+++ src/test/java/lombok/TestData.java
@@ -1,5 +1,7 @@
 package lombok;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -9,5 +11,13 @@ public class TestData {
     String name;
     String lastname;
 
-    List<String> lists = Arrays.asList("1111", "ddddd", "dd");
+    List<String> lists;
+
+
+    public TestData() {
+        this.name = "Miro_" + RandomStringUtils.randomAlphanumeric(10);
+        this.lastname = "Dydko_" + RandomStringUtils.randomAlphanumeric(10);
+        //this.lists = lists;
+        lists = Arrays.asList("nap1", "nap2", "nap3");
+    }
 }
