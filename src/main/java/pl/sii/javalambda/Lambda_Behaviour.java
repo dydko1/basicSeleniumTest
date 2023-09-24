27db9655728c586e870d37e1e825f37868a0930d
dydko <miroslaw.dyduch@gmail.com>
2023-09-24 08:48:35 +0200

    behaviour

---
+++ src/main/java/pl/sii/javalambda/Lambda_Behaviour.java
@@ -0,0 +1,38 @@
+package pl.sii.javalambda;
+
+public class Lambda_Behaviour {
+    public static void main(String[] args) {
+
+        IntCalculator intCalculator = (int x, int y) -> x * y;
+        engine(intCalculator);
+        engine((long x, long y) -> x + y);
+        engine((int x, int y) -> x + y);
+    }
+
+    private static void engine(LongCalculator calculator) {
+        long x = 2;
+        long y = 10;
+
+        long result = calculator.calculate(x, y);
+        System.out.println(result);
+    }
+
+    private static void engine(IntCalculator calculator) {
+        int x = 21;
+        int y = 10;
+
+        long result = calculator.calculate(x, y);
+        System.out.println(result);
+    }
+}
+
+
+@FunctionalInterface
+interface IntCalculator {
+    int calculate(int x, int y);
+}
+
+@FunctionalInterface
+interface LongCalculator {
+    long calculate(long x, long y);
+}
