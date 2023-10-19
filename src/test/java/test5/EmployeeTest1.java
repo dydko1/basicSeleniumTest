46b436dcac9a5317c1dc9c0980ae04f5efe0c72c
Mirosław Dyduch <m.dyduch@cez.gov.pl>
2023-10-19 12:36:08 +0200

    temp

--- src/test/java/test5/EmployeeTest1.java
+++ src/test/java/test5/EmployeeTest1.java
@@ -4,11 +4,14 @@ import org.testng.annotations.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.junit.Assert.assertEquals;
 
 public class EmployeeTest1 {
     private static Employee[] arrayOfEmps = {
@@ -17,6 +20,7 @@ public class EmployeeTest1 {
             new Employee(3, "Mark Zuckerberg", 300000.0)
     };
     private static List<Employee> empList = Arrays.asList(arrayOfEmps);
+    private static EmployeeRepository employeeRepository = new EmployeeRepository(empList);
 
     @Test
     public void whenIncrementSalaryForEachEmployee_thenApplyNewSalary() {
@@ -27,5 +31,16 @@ public class EmployeeTest1 {
                 hasProperty("salary", equalTo(220000.0)),
                 hasProperty("salary", equalTo(330000.0))
         ));
+    }
+
+    @Test
+    public void whenMapIdToEmployees_thenGetEmployeeStream() {
+        Integer[] empIds = {1, 2, 3};
+
+        List<Employee> employees = Stream.of(empIds)
+                .map(employeeRepository::findById)
+                .collect(Collectors.toList());
+
+        assertEqi   uals(employees.size(), empIds.length);
     }
 }
